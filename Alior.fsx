05da2bd0864665e6fbe05a48f8809305bc60a117
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2024-07-20 10:26:52 +0200

    refactor

--- Alior.fsx
+++ Alior.fsx
@@ -15,8 +15,8 @@ open clients.Alior
 let username () = System.Environment.GetEnvironmentVariable("ALIOR_USERNAME")
 let password () = System.Environment.GetEnvironmentVariable("ALIOR_PASSWORD")
 
-// let ac = AliorClient(username, password)
-let ac = AliorClient(username, password, p, true)
+let ac = AliorClient(username, password, true)
+// let ac = AliorClient(username, password, p, true)
 ac.SignIn()
 let p = ac.GetP()
 
@@ -27,5 +27,5 @@ let transfer = Transfers.Row("", "asdf", "84101000712221000000000000", "2024/Apr
 
 #load "Alior.fs"
 open clients.Alior
-let ac = AliorClient(username, password, p, true)
+// let ac = AliorClient(username, password, p, true)
 ac.TransferTax(transfer, taxOfficeName)
--- Utils.fs
+++ Utils.fs
@@ -8,17 +8,16 @@ open PuppeteerSharp
 module Utils =
     let sleep x = x |> TimeSpan.FromSeconds |> Thread.Sleep
 
-    let waitForSelectorAndClick (p:IPage) xpath =
-        p.WaitForSelectorAsync(xpath).Result.ClickAsync().Wait()
-
-    let waitForSelectorAndType (p:IPage) xpath text =
-        p.WaitForSelectorAsync(xpath).Result.TypeAsync(text).Wait()
-
     let wait (t:Task) = t.Wait()
+
     let runSync (t:Task<'a>) = t.Result
 
-    let clickElement (e:IElementHandle) =
-        e.ClickAsync() |> wait
+    let clickElement (e:IElementHandle) = e.ClickAsync() |> wait
 
     let clickSelector xpath (e:IElementHandle) =
-        e.WaitForSelectorAsync(xpath).Result.ClickAsync().Wait()
+        e.WaitForSelectorAsync(xpath) |> runSync |> clickElement
+
+    let waitForSelectorAndClick (p:IPage) xpath = p.WaitForSelectorAsync(xpath) |> runSync |> clickElement
+
+    let waitForSelectorAndType (p:IPage) xpath text =
+        p.WaitForSelectorAsync(xpath) |> runSync |> fun x -> x.TypeAsync(text) |> wait
