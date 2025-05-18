2e743853c55a7e0d0a992e205d297820a3091b68
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-05-18 15:04:33 +0200

    add fsx

---
+++ pgn.fsx
@@ -0,0 +1,29 @@
+#load "c:/git/prelude/prelude.fsx"
+
+Prelude.WorkingDirectorySetter.SetToMe()
+
+open System
+
+// --- header --- //
+
+#r "nuget: FSharp.Data, 6.4.0"
+#r "nuget: PuppeteerSharp, 18.0.3"
+
+#load "Utils.fs"
+#load "pgn.fs"
+
+open System.IO
+open System
+open PuppeteerSharp
+
+open clients
+open clients.Utils
+open clients.PGNIG
+
+let c = PGNiGClient(env "PGNIG_USERNAME", env "PGNIG_PASSWORD")
+c.SingIn()
+
+let is = c.ScrapeInvoicesInternal()
+
+c.ProcessInvoices(is)
+
