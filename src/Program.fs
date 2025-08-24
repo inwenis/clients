f540165b33b9a43a13f9214947e3628bb6c6e2a0
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-08-24 13:36:19 +0200

    wip

--- src/Program.fs
+++ src/Program.fs
@@ -1,6 +1,21 @@
 open System
+open Microsoft.Extensions.Configuration
 
 [<EntryPoint>]
 let main argv =
-    // Your code here
+    // Build configuration
+    let config =
+        ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional = false, reloadOnChange = true)
+            .Build()
+
+    // Read values
+    let greeting = config.["AppSettings:Greeting"]
+    let retryCount = config.["AppSettings:RetryCount"] |> int
+
+    // Use values
+    printfn "%s" greeting
+    printfn "RetryCount = %d" retryCount
+
     0
