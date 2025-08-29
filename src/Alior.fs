d4a914fe941869ac3003346946b7ac4670cec528
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-08-29 09:24:10 +0200

    align

--- src/Alior.fs
+++ src/Alior.fs
@@ -9,9 +9,9 @@ open FSharp.Data
 
 
 type Transfers = CsvProvider<"""FromAccount,ReceiverName,ReceiverAccount,TransferText,Amount,InsertedDateTime,Status,Type
-Billing account name is long to align with head,Receiver1,   12 1234 1234 12,Title of tra, 58.00,2023-03-01T00:00:00.0000000+02:00,ToBeExecuted,Regular
-Billing account name is long to align with head,Receiver3,   12 1234 1234 12,Title of tra, 12.52,2023-03-01T00:00:00.0000000+02:00,ToBeExecuted,Regular
-Billing account name is long to align with head,Receiver2,   12 1234 1234 12,Title of tra, 27.95,2023-03-01T00:00:00.0000000+02:00,Executed,Tax
+Billing account name is long to align      ,Receiver1   ,12 1234 1234 12,Title of tra, 58.00,2023-03-01T00:00:00.0000000+02:00,ToBeExecuted,Regular
+Billing account name is long to align      ,Receiver3   ,12 1234 1234 12,Title of tra, 12.52,2023-03-01T00:00:00.0000000+02:00,ToBeExecuted,Regular
+Billing account name is long to align      ,Receiver2   ,12 1234 1234 12,Title of tra, 27.95,2023-03-01T00:00:00.0000000+02:00,Executed,Tax
 """>
 
 
