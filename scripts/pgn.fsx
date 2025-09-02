5dc431a59c0acf33b892978c2896ba044e0f892a
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-09-02 10:47:55 +0200

    script changes - wip

--- scripts/pgn.fsx
+++ scripts/pgn.fsx
@@ -24,10 +24,14 @@ open PGNIG
 
 let args = [| "--disable-notifications"; "--force-device-scale-factor=0.9" |]
 
-
-let c = PGNiGClient(env "PGNIG_USERNAME", env "PGNIG_PASSWORD", args)
-c.SignIn()
+let mutable c = PGNiGClient(env "PGNIG_USERNAME", env "PGNIG_PASSWORD", args)
 let mutable p = c.GetP()
-// let x = p.QuerySelectorAsync("xpath///div").Result
+c.SignIn()
+
+#load "../src/Utils.fs"
+#load "../src/pgn.fs"
+open Utils
+open PGNIG
+c <- PGNiGClient(env "PGNIG_USERNAME", env "PGNIG_PASSWORD", page = p)
 
 c.ScrapeInvoices()
