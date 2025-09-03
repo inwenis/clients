af6573b5295c948fc2520bb91251f6d2b77dc839
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-09-03 22:56:47 +0200

    repro

--- scripts/Alior.fsx
+++ scripts/Alior.fsx
@@ -27,10 +27,13 @@ let c = AliorClient(env "ALIOR_USERNAME", env "ALIOR_PASSWORD", isTest=true)
 // open Alior
 // let c = AliorClient(env "ALIOR_USERNAME", env "ALIOR_PASSWORD", page = p, isTest=true)
 
-// let taxOfficeName = "Drugi Mazowiecki Urząd Skarbowy Warszawa"
-// let transferTax = Transfers.Row("", "asdf", "84101000712221000000000000", "2024/April", 123M, DateTimeOffset.UtcNow,  "asdf", "asdf")
-// let transferReg = Transfers.Row("", "asdf", "91113000070080239435200002", "asdf", 1M, DateTimeOffset.UtcNow,  "asdf", "asdf")
+let taxOfficeName = "Drugi Mazowiecki Urząd Skarbowy Warszawa"
+let transferTax = Transfers.Row("", "asdf", "84101000712221000000000000", "2024/April", 123M, DateTimeOffset.UtcNow,  "asdf", "asdf")
+let transferReg = Transfers.Row("Batorego 104", "asdf", "91113000070080239435200002", "asdf", 1M, DateTimeOffset.UtcNow,  "asdf", "asdf")
 
 c.SignIn()
+
+c.TransferRegular(transferReg)
+
 c.Scrape(period=All)
 let p = c.GetP()
