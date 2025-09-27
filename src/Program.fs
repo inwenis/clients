3e175f331ca4e5da22fe416044158fb454f53034
Filip Kucharczyk <fil.kucharczyk@gmail.com>
2025-09-27 10:58:07 +0200

    more tests

--- src/Program.fs
+++ src/Program.fs
@@ -25,14 +25,22 @@ let testAlior () =
 
 let testPGNIG args =
     let c = PGNiGClient(env "PGNIG_USERNAME", env "PGNIG_PASSWORD", args=args, isTest=true)
+
     c.SignIn()
     c.SubmitIndication 123
+    c.ScrapeInvoices() |> ignore
+    c.ScrapeOverpayments() |> ignore
+
+    printfn "if we reached this line without errors all must be good!"
 
 let testEnerga () =
     let c = EnergaClient(env "ENERGA_USERNAME", env "ENERGA_PASSWORD", isTest=true)
     c.SignIn()
     let d = c.SubmitIndication("a", 123)
     printfn "Submitted indication, received response: %A" d
+
+    printfn "if we reached this line without errors all must be good!"
+
 
 [<EntryPoint>]
 let main _ =
